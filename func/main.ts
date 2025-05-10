aac212bdb51a0116b2252bd26de069578429e4db
Mharc Nyvhie Guillermo <iamsuperv15@gmail.com>
2025-05-10 22:55:46 +0800

    debug

--- func/main.ts
+++ func/main.ts
@@ -2,27 +2,18 @@ import {
     readdirSync
 } from "fs";
 
-const typeArr = {
-    message: "text",
-    payload: "postback"
-};
-
 export async function resHandler
 (
     event: any,
     type: string
 ) {
-    const interpreter = typeArr[type];
-    const dataCategory = event[interpreter];
-    console.log(dataCategory)
-    const commandCategory = dataCategory[interpreter === typeArr[1] ? "payload" : interpreter];
+    const commandCategory = event[type];
 
-    readdirSync(`./processes/messenger/${interpreter}/response`)
+    readdirSync(`./processes/messenger/${type}/response`)
         .forEach((file: string) => {
-            if (!dataCategory) return;
             if (!commandCategory.includes(file.split(".")[0].toLowerCase())) return;
 
-            const command = import(`./../processes/messenger/${interpreter}/response/${file}`);
+            const command = import(`./../processes/messenger/${type}/response/${file}`);
 
             console.log(command)
         }
