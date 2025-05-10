68a44aa97436241243b3ff5e3ab7f61282dea564
Mharc Nyvhie Guillermo <iamsuperv15@gmail.com>
2025-05-11 05:21:48 +0800

    testing

--- src/func/main.ts
+++ src/func/main.ts
@@ -1,38 +1,6 @@
-import {
-    readFileSync,
-    readdirSync
-} from "fs";
 import * as message from "./message";
 import * as payload from "./payload";
 import { menu } from "./message";
-import {
-    resolve
-} from "path";
-import { Button } from "../assets/messenger";
-
-/*export async function resHandler
-(
-    event: any,
-    type: string
-) {
-    const commandCategory = event[type];
-
-     console.log(new Button().addTitle("test"), data)
-    console.log(readdirSync("/var/task/src/processes"));
-
-    readdirSync("/var/task/src/processes/messenger")
-        .forEach((file: string) => {
-            console.log(file)
-            if (!event[type].includes(file.split(".")[0].toLowerCase())) return;
-
-            const command: any = import(`./../processes/messenger/${type}/response/${file}`);
-
-            console.log(command)
-           
-            command.data.run(event, event.sender.id);;;
-        }
-    );
-};*/
 
 export async function resHandler
 (
