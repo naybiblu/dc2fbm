dd9befd884e9696287a8835c312a2f4469bc75b6
Mharc Nyvhie Guillermo <iamsuperv15@gmail.com>
2025-05-10 23:02:20 +0800

    debug

--- func/main.ts
+++ func/main.ts
@@ -9,7 +9,7 @@ export async function resHandler
 ) {
     const commandCategory = event[type];
 
-    readdirSync(`.`)
+    readdirSync(`__dir`)
         .forEach((file: string) => {
             console.log(file)
             if (!event[type].includes(file.split(".")[0].toLowerCase())) return;
