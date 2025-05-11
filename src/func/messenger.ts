7fdcc888cf6c90ed9ce3bfe0105b3bb3b15ae594
Mharc Nyvhie Guillermo <iamsuperv15@gmail.com>
2025-05-11 12:40:28 +0800

    test

--- src/func/messenger.ts
+++ src/func/messenger.ts
@@ -86,8 +86,8 @@ export async function FBhandler
         );
 
         console.log("eventType: " + Object.keys(event)[3]);
-        await mainHandler(event, Object.keys(event)[3]);
-        //await handleMessage(event.sender.id, event.message);
+        //await mainHandler(event, Object.keys(event)[3]);
+        await handleMessage(event.sender.id, event.message);
 
         return setTimeout(async() => { res.status(200).send("Event handled!") }, 50000);
     });
