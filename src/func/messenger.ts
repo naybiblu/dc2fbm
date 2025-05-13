be2ad956df139154bff1a07afe36bde52f5ae1e6
Mharc Nyvhie Guillermo <iamsuperv15@gmail.com>
2025-05-13 20:23:45 +0800

    debug

--- src/func/messenger.ts
+++ src/func/messenger.ts
@@ -80,7 +80,7 @@ export async function FBhandler
         const comparison = await compareMessages(sender, "created_time", 1, 2);
 
         //console.log(comparison )
-        //if (comparison.checkIfSame && comparison.first.uid !== sender) return res.status(200).send("Duplicate message.");
+        if (comparison.checkIfSame && comparison.first.uid !== sender) return res.status(200).send("Duplicate message.");
 
         //if (sender === pageId) return res.status(200).send("Bot reponse rejected for request!");
 
